c75195a97e9ee43ffd19389d8fbcf0bd40f48e7f
john <john@doe.com>
2025-02-14 14:17:16 +0000

    first commit

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  imports: [],
+  templateUrl: './my-component.component.html',
+  styleUrl: './my-component.component.scss'
+})
+export class MyComponentComponent {
+
+}
